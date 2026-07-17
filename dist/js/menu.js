0763e74d3f62fe4b88de968d7b2ed4c7c2648174
EstherCardote <estheer.correa@hotmail.com>
2026-07-17 16:29:22 -0300

    Criado a tela de lista de simulações

--- dist/js/menu.js
+++ dist/js/menu.js
@@ -185,7 +185,7 @@ function carregarMenu() {
                 <!-- SUBMENU -->
                 <ul class="hidden group-hover/simulador:block ml-12 mt-2 space-y-1 text-xs">
                     <li>
-                        <a href="#"
+                        <a href="./lista-simulados-salvos.html"
                             class="block p-2 text-white hover:bg-white/30 hover:text-slate-900 rounded-xl transition-opacity duration-300 whitespace-nowrap">Lista
                             de Simulações</a>
                     </li>
